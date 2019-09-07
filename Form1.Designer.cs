ac7af1da29280dc4fac735d16da3de7f82289b93
shadowfish <1633742366@qq.com>
2019-09-07 14:10:12 +0800

    窗体名称改变

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -234,7 +234,7 @@
             this.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "桌面文件整理";
             this.ResumeLayout(false);
             this.PerformLayout();
 
